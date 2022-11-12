f011b4bc3694d8c107b1a628a2ddd1362dbab560
Sriram Viswanathan <sriram.vish@gmail.com>
2022-11-11 21:43:30 -0500

    Extract out credentials read

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,14 @@
 import {visit} from "./visit_websites.js";
-import pkg from 'buttercup';
-import {env, exit} from "process";
-const { Credentials, FileDatasource, Vault, init } = pkg;
 import dotenv from "dotenv";
 import {login} from "./google_login.js";
 import vanillaPuppeteer, {PuppeteerLaunchOptions} from "puppeteer";
 import {addExtra} from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import {getProfile} from "./init-creds.js";
 
 dotenv.config()
 
-init()
-
-const datasourceCredentials = Credentials.fromDatasource({
-    path: "./user.bcup",
-    type: 'test',
-}, env["MASTER_PASSWORD"]);
-const fileDatasource = new FileDatasource(datasourceCredentials);
-
-// Read vault from disk (returns history and format)
-const vaultCredentials = Credentials.fromPassword("password marvel custom shine!");
-const loadedState = await fileDatasource.load(vaultCredentials);
-// Create a new vault instance from the loaded data
-const vault = Vault.createFromHistory(loadedState.history, loadedState.Format);
-
-vault.getAllGroups()
-
-const profile = env['PROFILE']
-
-if (profile === undefined) {
-    console.log("Profile is undefined!")
-    exit(0)
-}
-
-const entries = vault.findEntriesByProperty('title', profile)
-const thisProfile = entries[0]!;
+const thisProfile = await getProfile();
 
 // initialize puppeteer
 const options: PuppeteerLaunchOptions = {
