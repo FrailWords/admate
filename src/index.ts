f7adcad83407e96afa5c8ae141777863650ad867
Sriram Viswanathan <sriram.vish@gmail.com>
2022-11-13 12:37:08 -0500

    Clean up user data cache when running

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,38 @@ import vanillaPuppeteer, {PuppeteerLaunchOptions} from "puppeteer";
 import {addExtra} from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import {getProfile} from "./init-creds.js";
+import {env, exit} from "process";
+import path from "path";
+import rimraf from 'rimraf';
 
 dotenv.config()
 
-const thisProfile = await getProfile();
+const profile = env['PROFILE']
+
+if (profile === undefined) {
+    console.log("Profile is undefined!")
+    exit(0)
+}
+
+// Cleanup cache in profile data
+const profileDataPath = path.join('.', 'user-data', profile)
+rimraf.sync(path.join(profileDataPath, 'DevToolsActivePort'));
+rimraf.sync(path.join(profileDataPath, 'Default', 'Cache'));
+rimraf.sync(path.join(profileDataPath, 'Default', 'Code Cache'));
+rimraf.sync(path.join(profileDataPath, 'Default', 'DawnCache'));
 
 // initialize puppeteer
 const options: PuppeteerLaunchOptions = {
-    headless: false,
+    headless: true,
+    devtools: false,
     slowMo: 10, // slow down by 10ms
-    args: ['--disable-web-security', '--disable-features=IsolateOrigins,site-per-process']
+    args: ['--no-default-browser-check', '--disable-dev-shm-usage', `--user-data-dir=user-data/${profile}`]
 }
 const puppeteer = addExtra(vanillaPuppeteer);
 puppeteer.use(StealthPlugin());
 const browser = await puppeteer.launch(options);
+
+const thisProfile = await getProfile(profile);
 
 // Login into Google
 await login(browser, thisProfile);
